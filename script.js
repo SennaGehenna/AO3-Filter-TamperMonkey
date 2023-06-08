6aba0a6780f06ce07d66e1887c7ff24994ebee71
Anna Fall <thisistotallylegit@outlook.com>
2023-06-08 15:35:52 +0200

    fixed bug when no urlparams existed

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         AO3 auto-remove keywords
-// @namespace    sennagehenna.github.io
+// @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  a script that detects whether or not certain the "additional fields" filter is set to the desired keywords and reloads the page with the filter set
-// @author       Anna Fall
+// @author       You
 // @match        https://archiveofourown.org/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=archiveofourown.org
 // @grant        none
@@ -33,9 +33,6 @@
 
 
     function isSiteFor(reg) {
-		
-		//we get the URL of the current website and check if it contains the part "reg" that we supplied
-		
         return document.URL.match(RegExp(reg)) != null;
     }
 
@@ -47,8 +44,6 @@
     var bookmarksRegex = "/users.*/bookmarks.*"
     var searchRegex = "/works.*/search";
 
-
-    //we check if we're on an AO3 page that contains fanfics
     if(!isSiteFor(searchRegex) && (isSiteFor(worksRegex) || isSiteFor(tagsRegex) || isSiteFor(userRegex) || isSiteFor(bookmarksRegex))){
 
         var keyWords = filteredKeyWords.join();
@@ -58,9 +53,6 @@
 
         var isMissingParams = false;
 
-
-
-        // we check if everything we want to filter out is already being filtered or not
         if(excludedParams != null){
             for(var i = 0; i < filteredKeyWords.length; ++i){
                  if(excludedParams.match(RegExp(filteredKeyWords[i])) == null){
@@ -72,19 +64,27 @@
             isMissingParams = true;
         }
 
+        console.log("isMissingParams: "+isMissingParams);
 
-
-		//if the filter is incomplete, we add our custom filtered keywords to the filter and reload the page with the new filter
         if(isMissingParams){
 
             var newUrl = new URL(document.URL);
-            var newParams = newUrl.searchParams.get("work_search[excluded_tag_names]").split(",");
+            if(newUrl.searchParams == null){
+                newUrl.searchParams = new URLSearchParams();
+            }
+            var newParams = newUrl.searchParams.get("work_search[excluded_tag_names]");
+            if(newParams == null){
+                newParams = [];
+            }else {
+             newParams = newParams.split(",");
+            }
             for(var j = 0; j < filteredKeyWords.length; ++j){
                 if(newParams.indexOf(filteredKeyWords[j]) == -1){
                     newParams.push(filteredKeyWords[j]);
                 }
             }
             newUrl.searchParams.set("work_search[excluded_tag_names]",newParams.join());
+            console.log(newUrl.searchParams);
             window.location = newUrl;
         }
     }
